6d64547fcbb75c52c1d139dfeddaf0b93bc31ffc
Seven Du <shiweidu@outlook.com>
2017-11-27 10:03:15 +0800

    增加 PlusID 客户端列表程序。

---
+++ src/Models/Client.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SlimKit\PlusID\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'plus_id_clients';
+}
